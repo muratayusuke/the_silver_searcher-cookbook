02581412148a43d287d60a276fedab354877d779
Mark Cornick <mark@cornick.io>
2013-11-13 13:29:34 -0800

    Add chefspec as an experiment

---
+++ spec/recipes/default_spec.rb
@@ -0,0 +1,17 @@
+require_relative "../spec_helper"
+
+describe "the_silver_searcher::default" do
+  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+
+  %w(automake pkg-config zlib1g-dev libpcre3-dev liblzma-dev).each do |p|
+    it "installs #{p}" do
+      expect(chef_run).to install_package(p)
+    end
+  end
+
+  it "downloads and compiles the_silver_searcher" do
+    cache_file = "#{Chef::Config['file_cache_path']}/the_silver_searcher-#{chef_run.node.the_silver_searcher.version}.tar.gz"
+    expect(chef_run).to create_remote_file(cache_file)
+    expect(chef_run.remote_file(cache_file)).to notify("bash[install the_silver_searcher]").to(:run)
+  end
+end
