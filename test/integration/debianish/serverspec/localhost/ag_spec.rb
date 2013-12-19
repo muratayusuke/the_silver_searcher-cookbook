cdc51a5d7e5bc3918fe5e33675c2337d6163e4f3
Mark Cornick <mark@cornick.io>
2013-12-19 14:23:10 -0500

    fix brace style nit

--- test/integration/debianish/serverspec/localhost/ag_spec.rb
+++ test/integration/debianish/serverspec/localhost/ag_spec.rb
@@ -11,6 +11,6 @@ describe file("/usr/local/bin/ag") do
   it { should be_owned_by('root') }
   it { should be_grouped_into('root') }
   %w(owner group others).each do |executor|
-    it { should be_executable.by(executor)}
+    it { should be_executable.by(executor) }
   end
 end
