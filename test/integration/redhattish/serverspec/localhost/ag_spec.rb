fabb9d1537d15d3edb5c4edf959d44379255bbf6
Mark Cornick <mark@cornick.io>
2014-01-06 19:28:30 -0500

    Convert to expect syntax

--- test/integration/redhattish/serverspec/localhost/ag_spec.rb
+++ test/integration/redhattish/serverspec/localhost/ag_spec.rb
@@ -1,16 +1,24 @@
 require "spec_helper"
 
 %w(automake pkgconfig zlib zlib-devel pcre pcre-devel xz xz-devel).each do |prereq|
-  describe package(prereq) do
-    it { should be_installed }
+  describe "the #{prereq} package" do
+    it "should be installed" do
+      expect(package(prereq)).to be_installed
+    end
   end
 end
 
-describe file("/usr/local/bin/ag") do
-  it { should be_file }
-  it { should be_owned_by('root') }
-  it { should be_grouped_into('root') }
+ag = file("/usr/local/bin/ag")
+describe "/usr/local/bin/ag" do
+  it "should exist" do
+    expect(ag).to be_file
+  end
+  it "should be owned by root" do
+    expect(ag).to be_owned_by("root")
+  end
   %w(owner group others).each do |executor|
-    it { should be_executable.by(executor) }
+    it "should be executable by #{executor}" do
+      expect(ag).to be_executable.by(executor)
+    end
   end
 end
