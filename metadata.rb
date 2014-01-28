5395d80f73e21f086df1d2f7fc70b82255b8c440
Mark Cornick <mark@cornick.io>
2014-01-28 16:41:21 -0500

    Version bump to 1.3.4 (for development)

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          "MIT"
 description      "Installs/Configures The Silver Searcher"
 depends          "build-essential"
 depends          "chef-sugar"
-version          "1.3.3"
+version          "1.3.4"
 
 %w(debian ubuntu linuxmint raspbian redhat centos oracle scientific amazon
    enterpriseenterprise fedora).each do |platform|
