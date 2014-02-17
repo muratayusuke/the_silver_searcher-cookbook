4e96abc918d6a1c4f61c569a27cae60d12d6c5da
Mark Cornick <mark@cornick.io>
2014-02-17 15:49:05 -0500

    the_silver_searcher updated to version 0.19.2

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
-default.the_silver_searcher.version = '0.19.1'
-default.the_silver_searcher.checksum = '698cde7a8170ef1623f594d251aac846e720dc257c170f9c21102b3f2f1d4448'
+default.the_silver_searcher.version = '0.19.2'
+default.the_silver_searcher.checksum = 'e2de31b8b4745b4614d3a6fa4cd61130474b0e861e80b3784daee7ce0eeca300'
