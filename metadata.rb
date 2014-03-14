dea750f356fb77aa3c9cbcd435b54ea283927ccf
muratayusuke <info@muratayusuke.com>
2014-03-14 10:48:51 +0900

    update version

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'MIT'
 description      'Installs/Configures The Silver Searcher'
 depends          'build-essential'
 depends          'chef-sugar'
-version          '1.3.7'
+version          '1.3.7a'
 
 %w(debian ubuntu linuxmint raspbian redhat centos oracle scientific amazon
    enterpriseenterprise fedora).each do |platform|
